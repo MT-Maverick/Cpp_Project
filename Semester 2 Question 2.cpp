5d92501168b64f98b10fd17d0553ccd1a8ea082d
Mfundo Sindane <mfundosindane0@gmail.com>
2021-09-14 21:16:27 +0200

    Update Semester 2 Question 2.cpp

--- Semester 2 Question 2.cpp
+++ Semester 2 Question 2.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 using namespace std;
 
@@ -6,10 +5,16 @@ class node
 {
     public:
     string Name;
+    string Surname;
+    string Gender;
+    int Age;
     node* Next;
-    node(string name)
+    node(string name,string surname,string gender,int age)
     {
         this->Name=name;
+        this->Surname=surname;
+        this->Gender=gender;
+        this->Age=age;
         this->Next=NULL;
     }
 };
@@ -19,34 +24,26 @@ class list
   public:
   node* Head;
   void add_at_front(node* ptr)
-        {
+    {
         ptr->Next=Head;
         Head=ptr;
     }
-  bool is_empty()
-        {
-        if(Head==NULL)
-        {
-            return 1;
-        }
-        else
-        {
-            return 0;
-        }
-    }
+
   void add_at_end(node* ptr)
-        {
+    {
+        //creates the 2nd node:
       if(Head==NULL)
-      {
+        {
           Head=ptr;
           ptr->Next=NULL;
-      }
+        }
       else
-      {
+        {
           node* ptr2=get_last_node(); 
           ptr2->Next=ptr;
-      }
-  }
+        }
+    }
+    
     node* get_last_node()
         {
             node* ptr =Head;
@@ -57,18 +54,20 @@ class list
             }
             return ptr;
         }
-    node* search(string name)
+  
+    node* search(string name,string surname)
         {
             node* ptr=Head;
-            while(ptr!=NULL && ptr->Name!=name)
+            while(ptr!=NULL && (ptr->Name!=name && ptr->Surname!=surname))
             {
                 ptr=ptr->Next;
             }
             return ptr;
         }
-    node* remove_node(string name)
+  
+    node* remove_node(string name,string surname)
         {
-            node *ptr=search(name);
+            node *ptr=search(name,surname);
             node *x=Head;
             
             if(x==ptr)
@@ -86,12 +85,18 @@ class list
                 return ptr;
             }
         }
+  
   void print()
   {
     node *ptr=Head;
     while(ptr!=NULL)
     {
-        cout<<ptr->Name<<"->";
+        cout<<ptr->Name<<"\n"
+        <<ptr->Surname<<"\n"
+        <<ptr->Gender<<"\n"
+        <<ptr->Age<<"\n"
+        <<"-------------"<<endl;
+        
         ptr=ptr->Next;
     }
   }
@@ -103,13 +108,20 @@ class list
 int main()
 {
     list L;
-    node *n1 =new node("Mfundo");
-    node *n2 =new node("Trevor");
-    node *n3 =new node("Sindane");
+
+    node *n1 =new node("Mfundo","Sindane","Male",20);
+    node *n2 =new node("Richard","Rich","Female",30);
+    node *n3 =new node("Wanele","Ngwenya","Male",25);
+    node *n4 =new node("Keven","Hlangu","Female",18);
+    node *n5 =new node("Sihle","Mabena","Female",32);
+    
     L.add_at_front(n1);
     L.add_at_end(n2);
     L.add_at_end(n3);
+    L.add_at_end(n4);
+    L.add_at_end(n5);
     L.print();
-    cout<<wctype(n1);
+    L.remove_node("Keven","Hlangu");
+    L.print();
     return 0;
 }
